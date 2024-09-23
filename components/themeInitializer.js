541ac2100d88ab988fb260b17d703b25fd6b61fe
aakashsingh-maz <aakashsingh@zype.com>
2024-09-23 11:45:24 +0530

    remove theme btn

--- components/themeInitializer.js
+++ components/themeInitializer.js
@@ -4,8 +4,8 @@ import { useEffect } from 'react';
 
 function ThemeInitializer() {
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') || 'dark';
-    document.documentElement.setAttribute('data-bs-theme', savedTheme);
+    // const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
   }, []);
 
   return null;
